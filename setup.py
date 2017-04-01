3121bc57052b5adf9df05225b4a2b117481c5c45
Vivek Kumar Singh <vschs007@gmail.com>
2017-04-01 14:51:11 +0530

    updated
    space efficient

--- setup.py
+++ setup.py
@@ -26,15 +26,12 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
 from os import chdir
 from os.path import abspath, join, split
-
 # Make sure we are standing in the correct directory.
 # Old versions of distutils didn't take care of this.
 here = split(abspath(__file__))[0]
 chdir(here)
-
 # Package metadata.
 metadata = dict(
     name='google',
@@ -57,7 +54,6 @@ metadata = dict(
                  "Topic :: Software Development :: Libraries :: Python Modules",
                  ],
 )
-
 # Prefer setuptools over the old distutils.
 # If setuptools is available, use install_requires.
 try:
